b416248477f3d21f75147465ffa9899afa4fa987
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-07-23 09:44:27 +0530

    removed comments

--- src/main/java/com/travel/itinerary/dto/LoginRequest.java
+++ src/main/java/com/travel/itinerary/dto/LoginRequest.java
@@ -1,26 +1,20 @@
 package com.travel.itinerary.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class LoginRequest {
-    @NotBlank(message = "Username is required")
     private String userName;
-
-    @NotBlank(message = "email is required")
     private String email;
 
     @NotBlank(message = "Password is required")
     private String password;
 
-    // Manual getters and setters
     public String getUserName() { return userName; }
     public void setUserName(String userName) { this.userName = userName; }
     public String getEmail() { return email; }
