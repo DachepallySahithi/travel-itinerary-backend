858cb4ab4980de0b1d7636273ce3e12d5a86bc18
DachepallySahithi <130071155+DachepallySahithi@users.noreply.github.com>
2025-08-06 18:14:40 +0530

    updated

--- src/main/java/com/travel/itinerary/TravelApplication.java
+++ src/main/java/com/travel/itinerary/TravelApplication.java
@@ -1,7 +1,10 @@
 package com.travel.itinerary;
 
+import com.travel.itinerary.service.TextProcessingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class TravelApplication {
@@ -11,3 +14,14 @@ public class TravelApplication {
 	}
 
 }
+//public class TravelApplication {
+//	public static void main(String[] args) throws IOException{
+//		TextProcessingService textProcessingService = new TextProcessingService();
+//		String userQuery = "United states of America";
+//		String databaseDate = "United State of America";
+//		String ans = textProcessingService.processText(userQuery);
+//		String ans1 = textProcessingService.processText(databaseDate);
+//		System.out.println(ans);
+//		System.out.println(databaseDate);
+//	}
+//}
